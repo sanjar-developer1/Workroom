31db449bbf3834fd7eff498cfede5089dc7b04a7
sanjarcik <sanjarbekarabboyev22@gmail.com>
2025-09-04 16:28:29 +0500

    axios o'zgartirdim

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,13 +2,9 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://techinfo.uz/api",
-});
-
-export default api;
-    baseURL: "https://techinfo.uz/api",
-    headers: {
-        "Content-Type": "application/json"
-    }
+  headers: {
+    "Content-Type": "application/json"
+  }
 })
 
 export default api;
